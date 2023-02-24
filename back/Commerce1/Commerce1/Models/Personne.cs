2e7f7018b3dde88d2250dfb525fe2abf46000ffb
Oussama Aloui <oaloui@AD.FINSECUR.LAN>
2023-02-24 17:24:46 +0100

    ajout api .Net

---
+++ back/Commerce1/Commerce1/Models/Personne.cs
@@ -0,0 +1,17 @@
+﻿namespace Commerce1.Models
+{
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+        public Personne(string name, int age, string adress, int id)
+        {
+            Name = name;
+            Age = age;
+            Adress = adress;
+            Id = id ;
+        }
+    }
+}
