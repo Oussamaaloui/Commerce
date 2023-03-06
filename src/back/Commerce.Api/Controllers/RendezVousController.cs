f031a581d9e5ae6ee46e790a14a71686c60f2079
Selmi Abdelaziz <selmi.abdelaziz@gmail.com>
2023-03-06 04:40:29 +0100

    remove delay in rdv controller created previously for tests

--- src/back/Commerce.Api/Controllers/RendezVousController.cs
+++ src/back/Commerce.Api/Controllers/RendezVousController.cs
@@ -35,8 +35,6 @@ namespace Commerce.Api.Controllers
                 .Where(r => r.UserId == currentUserId)
                 .ToListAsync();
 
-             await Task.Delay(5000);
-
             return Ok(listeRendezVous.ToListViewModel());
         }
 
